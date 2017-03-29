3e9879fc4bc9d9b03841e17eee741348dfe8d9b8
Adam Bertalan <takeow.adam@gmail.com>
2017-03-29 19:28:20 +0200

    Initial modue for front view camera

---
+++ UniDebAutomatedCar/src/com/unideb/bosch/frontviewcamera/FrontViewCamera.java
@@ -0,0 +1,25 @@
+package com.unideb.bosch.frontviewcamera;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.unideb.bosch.automatedcar.framework.Signal;
+import com.unideb.bosch.automatedcar.framework.SystemComponent;
+
+public class FrontViewCamera extends SystemComponent {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	@Override
+	public void cyclic() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveSignal(Signal s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
