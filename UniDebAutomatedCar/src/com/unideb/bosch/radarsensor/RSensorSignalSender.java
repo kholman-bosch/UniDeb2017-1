ec7230e4ef04309492ba00782e63232f1ac3ed34
Zatesz <zboraiattila@gmail.com>
2017-04-03 15:14:06 +0200

    Preparing to connect RadarSensorSender with RadarSensor.

--- UniDebAutomatedCar/src/com/unideb/bosch/radarsensor/RSensorSignalSender.java
+++ UniDebAutomatedCar/src/com/unideb/bosch/radarsensor/RSensorSignalSender.java
@@ -8,21 +8,19 @@ import com.unideb.bosch.instrumentclusterdisplay.SignalDatabase;
 public class RSensorSignalSender {
 
 	private static int maximum_DetectableDangerousObjects = 5;
-	private static ArrayList<RSensorDetectedObjectAttributes> objectList;
+	private static ArrayList<RSensorDetectedObjectAttributes> objectList = new ArrayList<RSensorDetectedObjectAttributes>();;
 
-	private RSensorSignalSender(int max_DetectableDangerousObjects) {
-		maximum_DetectableDangerousObjects = max_DetectableDangerousObjects;
-		objectList = new ArrayList<RSensorDetectedObjectAttributes>();
+	private RSensorSignalSender() {
 	}
-	
-	//this method creates a list of most dangerous objects
+
+	// this method creates a list of most dangerous objects
 	private static void filterOut_LessDangerousObjects(RSensor radar) {
 		objectList.clear();
 		ArrayList<RSensorDetectedObjectAttributes> localList = radar.get_Detected_WorldObjects();
 		for (int i = 0; i < localList.size(); i++) {
 			// TODO: remove objects from list based on danger value
 			// objectList
-			//dangerValue
+			// dangerValue
 		}
 	}
 
@@ -31,14 +29,10 @@ public class RSensorSignalSender {
 		RSensorDetectedObjectAttributes actRadarObject;
 		for (int i = 0; i < objectList.size(); i++) {
 			actRadarObject = objectList.get(i);
-			VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.LONGITUDINAL_RELATIVE_VELOCITY,
-					(int) actRadarObject.longitudinalRelative_Velcit));
-			VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.LONGITUDINAL_DISTANCE_FROM_EGO,
-					(int) actRadarObject.longitudinalDistance_From_EGO));
-			VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.LATERAL_RELATIVE_VELOCITY,
-					(int) actRadarObject.longitudinalRelative_Velcit));
-			VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.LATERAL_DISTANCE_FROM_EGO,
-					(int) actRadarObject.lateralDistance_From_EGO));
+			VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.LONGITUDINAL_RELATIVE_VELOCITY, (int) actRadarObject.longitudinalRelative_Velcit));
+			VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.LONGITUDINAL_DISTANCE_FROM_EGO, (int) actRadarObject.longitudinalDistance_From_EGO));
+			VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.LATERAL_RELATIVE_VELOCITY, (int) actRadarObject.longitudinalRelative_Velcit));
+			VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.LATERAL_DISTANCE_FROM_EGO, (int) actRadarObject.lateralDistance_From_EGO));
 		}
 	}
 }
