152349bcd2617ffe6296c3abcd091d558c98371a
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-05 21:46:30 +0200

    Removed signal field added new ones and changed compareTo method.

--- UniDebAutomatedCar/src/com/unideb/bosch/frontviewcamera/RoadSign.java
+++ UniDebAutomatedCar/src/com/unideb/bosch/frontviewcamera/RoadSign.java
@@ -1,38 +1,66 @@
 package com.unideb.bosch.frontviewcamera;
 
-import com.unideb.bosch.automatedcar.framework.Signal;
-
 /**
  * @author Nandi
  * This class makes it possible to compare RoadSign objects by distance and then we can simply arrange them in a list
  * to get to 5 closest road sign with it's signal.
  *
  */
-public class RoadSign implements Comparable<Float> {
+public class RoadSign implements Comparable<RoadSign> {
 	
 	private final float distance;
-	private final Signal signal;
+	private final long longitudinalEGO;
+	private final long lateralEGO;
+	private final long trafficSignMeaing;
 	
-	public RoadSign(float distance, Signal signal) {
+	
+	
+
+	public RoadSign(float distance, long longitudinalEGO, long lateralEGO, long trafficSignMeaing) {
 		super();
 		this.distance = distance;
-		this.signal = signal;
+		this.longitudinalEGO = longitudinalEGO;
+		this.lateralEGO = lateralEGO;
+		this.trafficSignMeaing = trafficSignMeaing;
 	}
+
+
+	
 
 	public float getDistance() {
 		return distance;
 	}
 
-	public Signal getSignal() {
-		return signal;
+
+
+
+	public long getLongitudinalEGO() {
+		return longitudinalEGO;
 	}
 
+
+
+
+	public long getLateralEGO() {
+		return lateralEGO;
+	}
+
+
+
+
+	public long getTrafficSignMeaing() {
+		return trafficSignMeaing;
+	}
+
+
+
+
 	@Override
-	public int compareTo(Float o) {
-		if(o > this.distance){
+	public int compareTo(RoadSign o) {
+		if(o.getDistance() > this.distance){
 			return -1;
 		}
-		if(o < this.distance){
+		if(o.getDistance() < this.distance){
 			return 1;
 		}
 		return 0;
