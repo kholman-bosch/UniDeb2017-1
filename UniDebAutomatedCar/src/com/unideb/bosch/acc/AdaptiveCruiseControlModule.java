43a84d535b8d192e9827d28405776900e9c5849b
Adam Bertalan <takeow.adam@gmail.com>
2017-04-22 18:18:24 +0200

    [Feature] Adaptive Cruise Control - initial module creation. Trello card: https://trello.com/c/9zKcZxeF/2-feature-adaptive-cruise-control

---
+++ UniDebAutomatedCar/src/com/unideb/bosch/acc/AdaptiveCruiseControlModule.java
@@ -0,0 +1,20 @@
+package com.unideb.bosch.acc;
+
+import com.unideb.bosch.automatedcar.framework.Signal;
+import com.unideb.bosch.automatedcar.framework.SystemComponent;
+
+public class AdaptiveCruiseControlModule extends SystemComponent {
+
+	@Override
+	public void cyclic() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveSignal(Signal s) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
