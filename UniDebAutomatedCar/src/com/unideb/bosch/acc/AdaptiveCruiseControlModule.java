fc05f20d41128c25e2b16eca824b7c0331a9967f
Adam Bertalan <takeow.adam@gmail.com>
2017-04-24 09:28:47 +0200

    [Feature] Adaptive Cruise Control - Code refactor - trello card: https://trello.com/c/9zKcZxeF/2-feature-adaptive-cruise-control

--- UniDebAutomatedCar/src/com/unideb/bosch/acc/AdaptiveCruiseControlModule.java
+++ UniDebAutomatedCar/src/com/unideb/bosch/acc/AdaptiveCruiseControlModule.java
@@ -24,10 +24,9 @@ public class AdaptiveCruiseControlModule extends SystemComponent {
 	private float currentGasPedalPosition;
 	private float currentBrakePedalPosition;
 	
-	private float lastGasPedalPositionBeforeRelease;
-	
 	private float cruiseControlGasPedalPosition;
 	private float cruiseControlSpeed;
+	private float safeDistance;
 
 	@Override
 	public void cyclic() {
@@ -35,11 +34,10 @@ public class AdaptiveCruiseControlModule extends SystemComponent {
 		LOGGER.debug("Currently set cruise control speed: " + this.cruiseControlSpeed);
 		
 		if (this.accState.equals(AdaptiveCruiseControlState.ACTIVE)) {
-			if( this.currentSpeed <= this.cruiseControlSpeed ) {
-				this.cruiseControlGasPedalPosition += 1.0;
-			}
-			if( this.currentSpeed > this.cruiseControlSpeed ) {
-				this.cruiseControlGasPedalPosition -= 1.0;
+			if( Math.abs(this.currentSpeed - this.cruiseControlSpeed) < 1.0 ) {
+				fineTuneCruiseControlGasPedalPosition(0.1f);
+			} else {
+				fineTuneCruiseControlGasPedalPosition(1.0f);
 			}
 			
 			VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.GAS_PEDAL_POSITION, cruiseControlGasPedalPosition));
@@ -48,58 +46,85 @@ public class AdaptiveCruiseControlModule extends SystemComponent {
 
 	@Override
 	public void receiveSignal(Signal s) {
-
 		switch (s.getID()) {
 		case SignalDatabase.ACC_STATUS_CHANGED:
-			LOGGER.debug("ACC_STATUS_CHANGED SIGNAL RECEIVED: " + s.getData());
-			
-			if (s.getData() == 1.0f) {
-				LOGGER.debug("ACC ENABLE REQUEST");
-				if (this.currentSpeed >= MIN_REQ_SPEED_TO_ENABLE && this.currentSpeed <= MAX_REQ_SPEED_TO_ENABLE) {
-					LOGGER.debug("ACC IS NOW ENABLED");
-					this.accState = AdaptiveCruiseControlState.ACTIVE;
-					// TODO instead of capturing the gas pedal position and freeze it, set it manually for the speed
-					this.cruiseControlGasPedalPosition = this.lastGasPedalPositionBeforeRelease; // cruise controling
-					this.cruiseControlSpeed = this.currentSpeed;
-				} else {
-					LOGGER.debug("CANNOT ENABLE ACC DUE TO INAPPROPRIATE SPEED");
-					this.accState = AdaptiveCruiseControlState.DISABLED; // disable acc
-					VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.ACC_STATUS_CHANGED, 0.0f));
-				}
-			} else {
-				LOGGER.debug("ACC DISABLE REQUEST");
-				this.accState = AdaptiveCruiseControlState.DISABLED; // disable acc
-//				VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.ACC_STATUS_CHANGED, 0.0f));
-			}
+			onReceiveAccStatusChangeSignal(s);
 			break;
 		case SignalDatabase.VEHICLE_SPEED:
-			System.out.println("SPEED: " + s.getData());
-			this.currentSpeed = s.getData();
+			onReceiveVehicleSpeedSignal(s);
 			break;
 		case SignalDatabase.GAS_PEDAL_POSITION:
-			System.out.println("GAS PEDAL POSITION: " + s.getData());
-			System.out.println("CRUISE CONTROL GAS PEDAL POSITION: " + this.cruiseControlGasPedalPosition);
-			this.currentGasPedalPosition = s.getData();
-			
-			if( s.getData() != 0.0 ){
-				this.lastGasPedalPositionBeforeRelease = s.getData();
-			}
-			
-			// ACC is enabled, right now cruise controling, but gas pedal pressed
-			if( this.accState == AdaptiveCruiseControlState.ACTIVE && s.getData() != this.cruiseControlGasPedalPosition && s.getData() != 0.0){
-				this.accState = AdaptiveCruiseControlState.SUSPENDED;
-			}
-			
+			onReceiveGasPedalPositionSignal(s);
 			break;
 		case SignalDatabase.BRAKE_PEDAL_POSITION:
-			System.out.println("BRAKE PEDAL POSITION: " + s.getData());
-			this.currentBrakePedalPosition = s.getData();
-			
-			if( this.accState == AdaptiveCruiseControlState.ACTIVE && s.getData() != 0.0){
-				this.accState = AdaptiveCruiseControlState.SUSPENDED;
-			}
+			onReceiveBrakePedalPositionSignal(s);
 			break;
 		}
+	}
+	
+	private void fineTuneCruiseControlGasPedalPosition( float step ) {
+		if( this.currentSpeed <= this.cruiseControlSpeed ) {
+			this.cruiseControlGasPedalPosition += step;
+		}
+		if( this.currentSpeed > this.cruiseControlSpeed ) {
+			this.cruiseControlGasPedalPosition -= step;
+		}
+	}
+	
+	private void onReceiveAccStatusChangeSignal(Signal s) {
+		LOGGER.debug("ACC_STATUS_CHANGED SIGNAL RECEIVED: " + s.getData());
+		
+		if (s.getData() == 1.0f) {
+			LOGGER.debug("ACC ENABLE REQUEST");
+			if (this.currentSpeed >= MIN_REQ_SPEED_TO_ENABLE && this.currentSpeed <= MAX_REQ_SPEED_TO_ENABLE) {
+				enableACC();
+			} else {
+				LOGGER.debug("CANNOT ENABLE ACC DUE TO INAPPROPRIATE SPEED");
+				disableACC();
+				VirtualFunctionBus.sendSignal(new Signal(SignalDatabase.ACC_STATUS_CHANGED, 0.0f));
+			}
+		} else {
+			LOGGER.debug("ACC DISABLE REQUEST");
+			disableACC();
+		}
+	}
+	
+	private void onReceiveVehicleSpeedSignal(Signal s) {
+		System.out.println("SPEED: " + s.getData());
+		this.currentSpeed = s.getData();
+	}
+	
+	private void onReceiveGasPedalPositionSignal(Signal s) {
+		System.out.println("GAS PEDAL POSITION: " + s.getData());
+		System.out.println("CRUISE CONTROL GAS PEDAL POSITION: " + this.cruiseControlGasPedalPosition);
+		this.currentGasPedalPosition = s.getData();
+		
+		// ACC is enabled, right now cruise controling, but gas pedal pressed
+		if( this.accState == AdaptiveCruiseControlState.ACTIVE && s.getData() != this.cruiseControlGasPedalPosition && s.getData() != 0.0){
+			this.accState = AdaptiveCruiseControlState.SUSPENDED;
+		}
+	}
+	
+	private void onReceiveBrakePedalPositionSignal(Signal s) {
+		System.out.println("BRAKE PEDAL POSITION: " + s.getData());
+		this.currentBrakePedalPosition = s.getData();
+		
+		if( this.accState == AdaptiveCruiseControlState.ACTIVE && s.getData() != 0.0){
+			this.accState = AdaptiveCruiseControlState.SUSPENDED;
+		}
+	}
+	
+	private void enableACC() {
+		LOGGER.debug("ACC IS NOW ENABLED");
+		this.accState = AdaptiveCruiseControlState.ACTIVE;
+		// start cruise controlling
+		this.cruiseControlSpeed = this.currentSpeed;
+		this.cruiseControlGasPedalPosition = this.cruiseControlSpeed / 120.0f * 100;
+	}
+	
+	private void disableACC() {
+		LOGGER.debug("ACC IS NOW DISABLED");
+		this.accState = AdaptiveCruiseControlState.DISABLED;
 	}
 
 }
